85ba246b691d2dca23a78a1daff3dd4f3cd5b619
beloenko2-png <beloenko2@gmail.com>
2025-12-12 15:19:42 +0300

    Add files via upload

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+
+my_user = User("Иван", "Иванов")
+
+my_user.print_first_name()    # Выведет: Имя: Иван
+my_user.print_last_name()     # Выведет: Фамилия: Иванов
+my_user.print_full_name() 
